5052ebfd7303ebeba143a60e3b98568df6edeca7
Siarhei.Baltrukevich <boltic28@gmail.com>
2021-11-24 16:21:26 +0300

    Initial commit

---
+++ app/src/main/java/com/boltic28/carassistant/Application.kt
@@ -0,0 +1,9 @@
+package com.boltic28.carassistant
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+}
---
+++ app/src/main/java/com/boltic28/carassistant/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.boltic28.carassistant.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule
